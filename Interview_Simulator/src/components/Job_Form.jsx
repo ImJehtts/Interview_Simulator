13ae9e4f0aeaea1de6e2a95b8efebee31ac55ba3
Mohib Rehman <rehmanmohib08@gmail.com>
2024-12-02 10:18:06 -0500

    Responsive and inputs boxes fixed
    The page now works on desktop and laptops (at least those I tested) and changes accordingly. The buttons also are styled and work as needed. The input boxes can also take information. Next I think is double checking responsiveness and then being able to actually submit inputs with saving to state. Then hopefully we can start other parts.

--- Interview_Simulator/src/components/Job_Form.jsx
+++ Interview_Simulator/src/components/Job_Form.jsx
@@ -8,30 +8,35 @@ function Job_Description({pressedNext, totalSteps, currentStep}){
     const [jobRequirements, setJobRequirements] = useState('')
     
     return (
-    <div className="form-container">
-        <h2>About Job</h2>
-        <form> 
-            <input 
-            type="text" 
-            placeholder="What is the Job Title (eg. Software Engineering Intern)"
-            />
-            <input type="text" placeholder="What is the Company Name (eg. Mohib's Software Inc.)"></input>
-            <input type="text" placeholder="What is the Job Description (eg. The 'What we do' and/or 'What you will do' sections)" className="description_section"></input>
-            <input type="text" placeholder="What are the Job Requirments (eg. Skills/Requirments section (Python, Bachelors, ...))" className="requirments_section"></input>
-        </form>
-        <div className='bts' style={{marginTop: "-15.3rem",}}>
+        <div>
+        <div className="form-container">
+            <h2>About Job</h2>
+            <form> 
+                <input 
+                type="text" 
+                placeholder="What is the Job Title (eg. Software Engineering Intern)"
+                />
+                <input type="text" placeholder="What is the Company Name (eg. Mohib's Software Inc.)"></input>
+                <input type="text" placeholder="What is the Job Description (eg. The 'What we do' and/or 'What you will do' sections)" className="description_section"></input>
+                <input type="text" placeholder="What are the Job Requirments (eg. Skills/Requirments section (Python, Bachelors, ...))" className="requirments_section"></input>
+            </form>
+        </div>
+        {/*         */}
+        
+        <div className='submitbtn'>
             <button className='btn'>Submit</button>
         </div>
+
         <div className='results'>
-            <div className='results_5'>
-                <h3>Results: You will get a rating out of /5 for your response</h3>
-            </div>
-            <div className='results_reedback'>
-                <h4>Feedback: This is where you will see feedback when you complete a question</h4>
-            </div>
-            <div className='bts'> 
-            <button className={`${currentStep === totalSteps ? "disabled":"btn"}`} onClick={pressedNext}>Next</button>
-            </div>
+                <div className='results_5'>
+                    <h3>Results: You will get a rating out of /5 for your response</h3>
+                </div>
+                <div className='results_reedback'>
+                    <h4>Feedback: This is where you will see feedback when you complete a question</h4>
+                </div>
+                <div> 
+                <button className={`${currentStep === totalSteps ? "disabled":"btn"}`} onClick={pressedNext}>Next</button>
+                </div>
         </div>
     </div>
     )
