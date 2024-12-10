38329b015ed9911432ef0f3d363370093be3d096
Mohib Rehman <rehmanmohib08@gmail.com>
2024-12-09 19:07:55 -0500

    Results component fixes
    Removed steps from components because they have no use for it.
    Results components and values now dynamically change.
    Next button in Results can not be pressed unless job information is filled out.

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
-const Results = ({pressedNext, totalSteps, currentStep}) => {
+const Results = ({pressedNext, rating, feedback, cangonext}) => {
+  const onClickfunction = cangonext === 0 ? null : pressedNext
+
   return (
     <div className='results'>
         <div className='results_5'>
-            <h3>Results: You will get a rating out of /5 for your response</h3>
+            <h3>Results: {rating}</h3>
         </div>
         <div className='results_reedback'>
-            <h4>Feedback: This is where you will see feedback when you complete a question</h4>
+            <h4>Feedback: {feedback}</h4>
         </div>
         <div> 
-        <button className={`${currentStep === totalSteps ? "disabled":"btn"}`} onClick={pressedNext}>Next</button>
+        <button className={`${cangonext === 0 ? "disabled":"btn"}`} onClick={onClickfunction}>Next</button>
         </div>
     </div>
   )
