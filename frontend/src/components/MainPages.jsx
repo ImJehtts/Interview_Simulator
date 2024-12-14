3cc990e97b8ca1b665d1b52116ab91e5ec56cb53
Mohib Rehman <rehmanmohib08@gmail.com>
2024-12-14 05:47:40 -0500

    Behavioral CSS and global rating
    There is now a global rating which gets updated so at the end we can get an overall rating
    
    CSS fixing for behvioural so looks correct after question and feedback are given

--- frontend/src/components/MainPages.jsx
+++ frontend/src/components/MainPages.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import ProgressBar from './ProgressBar'
 import Job_Description from './Job_Form'
 import Behavioural1 from './Behavioural1'
+import Behavioural2 from './Behavioural2.jsx'
 
 
 
 function MainPages(){
     const [currentStep, setcurrentStep] = useState(0)
     const [jobdata, setjobData] = useState('')
+    const [overallRating, setoverallRating] = useState(0)
     const totalSteps = 6
 
     function pressedNext() {
@@ -18,11 +20,15 @@ function MainPages(){
         setjobData(jobdata)
     }
 
+    const overallRatingtoMain = (rating) => {
+        setoverallRating((prevRating) => prevRating + rating);
+    }
+
     const renderSteps = () =>{
         switch (currentStep){
             case 0: return <Job_Description pressedNext={pressedNext} jobDatatoMain={jobDatatoMain}/>;
-            case 1: return <Behavioural1 pressedNext={pressedNext} jobData={jobdata}/>;
-            case 2: return <Job_Description pressedNext={pressedNext} totalSteps={totalSteps} currentStep={currentStep}/>;
+            case 1: return <Behavioural1 pressedNext={pressedNext} jobData={jobdata} overallRatingtoMain={overallRatingtoMain}/>;
+            case 2: return <Behavioural2 pressedNext={pressedNext} jobData={jobdata} overallRatingtoMain={overallRatingtoMain}/>;
             case 3: return <Job_Description pressedNext={pressedNext} totalSteps={totalSteps} currentStep={currentStep}/>;
             case 4: return <Job_Description pressedNext={pressedNext} totalSteps={totalSteps} currentStep={currentStep}/>;
             case 5: return <Job_Description pressedNext={pressedNext} totalSteps={totalSteps} currentStep={currentStep}/>;
@@ -35,7 +41,7 @@ function MainPages(){
         <div className='progress_container'>
             <ProgressBar totalSteps={totalSteps} currentStep={currentStep} className='progress'/>
             <div className={`${currentStep >= 0 ? "circle active":"circle"}`}>About Job</div>
-            <div className={`${currentStep >= 1? "circle active":"circle"}`}>Behavioural #1</div>
+            <div className={`${currentStep >= 1 ? "circle active":"circle"}`}>Behavioural #1</div>
             <div className={`${currentStep >= 2 ? "circle active":"circle"}`}>Behavioural #2</div>
             <div className={`${currentStep >= 3 ? "circle active":"circle"}`}>Skills</div>
             <div className={`${currentStep >= 4 ? "circle active":"circle"}`}>Techincal Code</div>
